d909652540a47fc034262f1afdd591dcd2ef3088
George Dong <georgedong@Georges-Mac.local>
2017-11-05 14:02:18 -0500

    set current nav different bk color

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,28 @@ var elTime = document.getElementById('time');
 function updateTime(){
   elTime.innerHTML = (new Date).toLocaleString();
 }
+
+function setCurTag(){
+  id = '';
+  switch(window.location.pathname){
+    case '/':
+      id = 'nav_home';
+      break;
+    case '/products':
+      id = 'nav_products';
+      break;
+    case '/contact':
+      id = 'nav_contact';
+      break;
+    case '/edit':
+      id = 'nav_edit';
+      break;
+  }
+  document.getElementById(id).classList.add('nav_tag_cur')
+}
+
 window.onload=function(){
 updateTime();
+setCurTag();
 setInterval(updateTime,1000);
 };
